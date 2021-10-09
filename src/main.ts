dbafb0673687c36501374936f474df9a472279a0
Matthew Slauson <slauson.matthew@gmail.com>
2021-10-08 23:34:55 -0500

    renaming

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './blog.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
